4917047448e6b095a4623cbc99701dbf76d3482e
Alex J. Gustafson <alexjgustafson@gmail.com>
2021-08-28 19:54:30 -0400

    Save shell commands to bundle the plugin

---
+++ build.php
@@ -0,0 +1,8 @@
+<?php
+namespace Ajg;
+
+// Build the block
+shell_exec('cd blocks/ajg-native-lands-search/ && npm run build');
+
+// Zip the plugin
+shell_exec('cd .. && zip -r ajg-native-lands.zip ajg-native-lands  -x *.git* *.gitignore* *node_modules* *package-lock.json* *package.json* *gulpfile.js*');
